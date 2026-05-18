0f0ae086ce1ca1699a0012410c835c4701730aca
Thomas <thomas.cifuentes@udla.edu.ec>
2026-05-18 12:10:42 -0500

    definicion de prototipo

--- main.c
+++ main.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
-int main(){
-float v[5]={6,7,7.5,6.5,8};
+float calcularPromedio(float *vector, int tamanio);
+int main()
+{
+float notas[5]={6,7,7.5,6.5,8};
+int tamanio=5;
 for(int i=0; i< 5; i++)
 {
-    printf("%.2f",v[i]);
+    printf("%.2f",notas[i]);
 }
+    calcularPromedio(notas,tamanio);
 
 }
