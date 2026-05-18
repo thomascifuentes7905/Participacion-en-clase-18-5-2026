b607868c500ce898cbfbb665cd1e9cb2bc76f082
Thomas <thomas.cifuentes@udla.edu.ec>
2026-05-18 12:20:20 -0500

    Version final

--- main.c
+++ main.c
@@ -9,5 +9,15 @@ for(int i=0; i< 5; i++)
     printf("%.2f",notas[i]);
 }
     calcularPromedio(notas,tamanio);
-
+    printf("El promedio es: %.2f", calcularPromedio (notas, tamanio));
+}
+float calcularPromedio(float *vector, int tamanio)
+{
+    float suma, promedio=0;
+    for (int i=0; i< tamanio; i++)
+    {
+        suma = suma + vector[i];
+    }
+    promedio = suma / tamanio;
+    return promedio;
 }
